aef66a04c4417c610112a2f71935bf6ba490bcf6
Tina Ye <tina.ye@arc90.com>
2012-03-12 13:26:54 -0400

    updated database schema doc

--- hotpot_mongo.js
+++ hotpot_mongo.js
@@ -1,4 +1,4 @@
-A MEAL = {
+a meal = {
 	"_id": ObjectId("42353a3c3b..."),
 	"title": "kale and..",
 	"slug" : "LemonGarlicKalePasta",
@@ -18,45 +18,27 @@ A MEAL = {
 	],
 }
 
-/* 
-A SNIPPET = {
-	"_id": "123123",
-	"type": "protip" (or "foodnote"),
-	"text": "bust out...",
-} */
-
-A USERNOTE = {
+a cookingNote = {
 	"_id": ObjectId("234234lkas897"),
-	"user_id": "4123bac0132...",
-	"recipe_id": "42353a3c3b...",
-	"snippet_id": "123123",
-	"text": "hallo"
+	"userId": "4123bac0132...",
+	"invitationId": "4123bac0132...", // invitation referrs to the cooking room where this note was posted
+	"stepId" : "9",
+	"type": "note", // or badge, etc.
+	"content": "hallo, this kale is delicious",
+	"timestamp": 1330483597
 }
 
-A USER = {
+a user = {
 	"_id": ObjectId("4123bac0132..."),
 	"email" : "t@tinabeans.com",
 	"name" : "Tina",
 	"password" : "somethingencrypted",
 	"location" : "New York",
-	"userpic" : "4f2e0b279685aa1618000001_0.235118283529.png"
+	"userpic" : "4f2e0b279685aa1618000001_0.235118283529.png" // stored in uploads/userpics
 }
 
-A ROOM = {
-	"_id" : ObjectID("4123bac0132..."),
-	"recipe_id" : "42353a3c3b...",
-	"users" : [ "adfadfasdf", "asdfasdfasdf", ...]
-}
-
-A BADGE GIVEN = {
-	"giverId": "e@mail.com",
-	"receiverId": "t@tinabeans.com",
-	"badgeSlug": "42353a3c3b…",
-	"roomId": "4123bac0132…",
-	"stepId": "9"
-}
-
-AN INVITATION = {
+// also represents a room when the invitation is accepted
+an invitation = {
 	"_id" : ObjectId("4123bac0132..."),
 	"status" : "new",
 	"hostId" : "4123bac0132...",
@@ -81,7 +63,28 @@ AN INVITATION = {
 
 
 
+/******************************************************************************/
+/* OLD/UNUSED */
 
+a room = {
+	"_id" : ObjectID("4123bac0132..."),
+	"recipe_id" : "42353a3c3b...",
+	"users" : [ "adfadfasdf", "asdfasdfasdf", ...]
+}
+
+a badge given = {
+	"giverId": "e@mail.com",
+	"receiverId": "t@tinabeans.com",
+	"badgeSlug": "42353a3c3b…",
+	"roomId": "4123bac0132…",
+	"stepId": "9"
+}
+
+a snippet = {
+	"_id": "123123",
+	"type": "protip" (or "foodnote"),
+	"text": "bust out...",
+}
 
 
 
